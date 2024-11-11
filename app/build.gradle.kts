03ce72f1a1d4e3eed6bcb54cc42f14613a5baa2a
JoRioW <rio.wicaksono56@gmail.com>
2024-11-11 17:06:09 +0700

    added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,4 +50,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
     implementation("com.github.bumptech.glide:glide:4.14.2")
+
+    val lifecycle_version = "2.8.7"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
 }
